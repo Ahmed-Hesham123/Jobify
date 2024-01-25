504326a643b315818cb33133f2993e6a13016baa
Ahmed Hesham <ahmedh246891@gmail.com>
2024-01-26 00:47:03 +0200

    add landing page ui

--- client/src/pages/Landing.jsx
+++ client/src/pages/Landing.jsx
@@ -1,9 +1,36 @@
+import Wrapper from "../assets/wrappers/LandingPage";
+import main from "../assets/images/main.svg";
+import { Link } from "react-router-dom";
+import { Logo } from "../components/index";
+
 const Landing = () => {
-    return (
-        <div>
-            Landing
+  return (
+    <Wrapper>
+      <nav>
+        <Logo />
+      </nav>
+      <div className="container page">
+        <div className="info">
+          <h1>
+            job <span>tracking</span> app
+          </h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis,
+            sit fuga sapiente, veritatis voluptatibus eveniet nihil delectus
+            neque voluptatem impedit tempora et suscipit eos vero, amet
+            blanditiis aliquid iusto. Reprehenderit.
+          </p>
+          <Link to="/register" className="btn register-link">
+            Register
+          </Link>
+          <Link to="/login" className="btn">
+            Login / Demo User
+          </Link>
         </div>
-    );
-}
+        <img src={main} alt="job hunt" className="img main-img" />
+      </div>
+    </Wrapper>
+  );
+};
 
 export default Landing;
